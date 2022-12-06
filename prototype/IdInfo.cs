f2e5ed29a68ae9f860740c25073fcd9b1a0db88b
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-06 15:53:26 -0300

    prototype implementation

---
+++ prototype/IdInfo.cs
@@ -0,0 +1,11 @@
+namespace prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber { get; set; }
+        public IdInfo(int IdNumber)
+        {
+            this.IdNumber = IdNumber;
+        }
+    }
+}

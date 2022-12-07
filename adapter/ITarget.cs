b58e62640e692762a26f9c959cdc5fce8bfde6f7
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-07 10:35:49 -0300

    adapter example code

---
+++ adapter/ITarget.cs
@@ -0,0 +1,8 @@
+namespace adapter
+{
+    // The Target defines the domain-specific interface used by the client code.
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+}
---
+++ adapter/Program.cs
@@ -0,0 +1,8 @@
+﻿using adapter;
+
+Adaptee adaptee = new Adaptee();
+ITarget target = new Adapter(adaptee);
+
+Console.WriteLine("Adaptee interface is incompatible with the client.");
+Console.WriteLine("But with adapter client can call it's method.");
+Console.WriteLine(target.GetRequest());

8fd0dfa3db88db3c7548173ceae0294aade19453
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-07 13:57:40 -0300

    bridge example code

---
+++ bridge/ConcreteImplementationB.cs
@@ -0,0 +1,10 @@
+namespace bridge
+{
+    public class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform B.\n";
+        }
+    }
+}
---
+++ bridge/Program.cs
@@ -0,0 +1,14 @@
+﻿using bridge;
+
+Client client = new Client();
+
+Abstraction abstraction;
+// The client code should be able to work with any pre-configured
+// abstraction-implementation combination.
+abstraction = new Abstraction(new ConcreteImplementationA());
+client.ClientCode(abstraction);
+
+Console.WriteLine();
+            
+abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+client.ClientCode(abstraction);

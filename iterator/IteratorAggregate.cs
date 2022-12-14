573bb3e1d5cc20d9bdfddd79de7d9f7935b811a9
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-14 14:55:42 -0300

    iterator example code

---
+++ iterator/IteratorAggregate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+
+namespace iterator
+{
+    abstract class IteratorAggregate : IEnumerable
+    {
+        // Returns an Iterator or another IteratorAggregate for the implementing
+        // object.
+        public abstract IEnumerator GetEnumerator();
+    }
+}

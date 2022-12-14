6adfcbcc0a537639080a628446ee673242cd4370
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-14 15:10:39 -0300

    mediator example code

---
+++ mediator/BaseComponent.cs
@@ -0,0 +1,19 @@
+namespace mediator
+{
+    // The Base Component provides the basic functionality of storing a
+    // mediator's instance inside component objects.
+    class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            this._mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+    }
+}

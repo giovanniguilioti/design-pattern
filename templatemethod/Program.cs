00a760cca25ae19fe1ff215c2c2bf700177b2b9e
giovanniguilioti <ngiovanni.guilioti@gmail.com>
2022-12-21 14:04:45 -0300

    template method example code

---
+++ templatemethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using templatemethod;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Same client code can work with different subclasses:");
+
+        Client.ClientCode(new ConcreteClass1());
+
+        Console.Write("\n");
+            
+        Console.WriteLine("Same client code can work with different subclasses:");
+        Client.ClientCode(new ConcreteClass2());
+    }
+}
